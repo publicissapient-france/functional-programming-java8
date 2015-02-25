dc549374e19a41317574e1afd51603b515b0532a
ibeauvais <ivan.beauvais@gmail.com>
2015-02-25 13:42:03 +0100

    add step0 method for Consumer, Supplier, BiFunction and static method reference

--- src/main/java/fr/xebia/java8/refactoring/step0/FunctionGenerator.java
+++ src/main/java/fr/xebia/java8/refactoring/step0/FunctionGenerator.java
@@ -1,14 +1,13 @@
 package fr.xebia.java8.refactoring.step0;
 
-import java.util.Date;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.util.function.*;
 
 public class FunctionGenerator {
 
     public static Function<Integer, Integer> plusOneFunction() {
-
+        //TODO: replace with lambda syntax
         return new Function<Integer, Integer>() {
 
             @Override
@@ -19,6 +18,7 @@ public class FunctionGenerator {
     }
 
     public static Predicate<Integer> evenNumberPredicate() {
+        //TODO: replace with lambda syntax
         return new Predicate<Integer>() {
             @Override
             public boolean test(Integer integer) {
@@ -27,11 +27,49 @@ public class FunctionGenerator {
         };
     }
 
-    public static Supplier<Date> todaySupplier() {
-        return new Supplier<Date>() {
+    public static Consumer<int[]> incrementArrayValueConsumer() {
+        //TODO: replace with lambda syntax
+        return new Consumer<int[]>() {
             @Override
-            public Date get() {
-                return new Date();
+            public void accept(int[] arrayValues) {
+                for (int i = 0; i < arrayValues.length; i++) {
+                    arrayValues[i] = arrayValues[i] + 1;
+
+                }
+            }
+        };
+    }
+
+    public static Supplier<Integer> todaySupplier() {
+        //TODO: replace with lambda syntax
+        return new Supplier<Integer>() {
+            @Override
+            public Integer get() {
+                return LocalDate.now().get(ChronoField.DAY_OF_MONTH);
+
+            }
+        };
+    }
+
+    public static BiFunction<String, Integer, String> concatAsStringFunction() {
+        //TODO: replace with lambda syntax
+        return new BiFunction<String, Integer, String>() {
+
+            @Override
+            public String apply(String firstValue, Integer secondValue) {
+                return firstValue + " " + secondValue;
+            }
+        };
+    }
+
+    //BinaryOperator is BiFunction with same generic type
+    public static BinaryOperator<Integer> sumFunction() {
+        //TODO: replace with static methode reference
+        return new BinaryOperator<Integer>() {
+
+            @Override
+            public Integer apply(Integer firstValue, Integer secondValue) {
+                return Integer.sum(firstValue, secondValue);
             }
         };
     }
--- src/test/java/fr/xebia/java8/refactoring/step0/FunctionGeneratorTest.java
+++ src/test/java/fr/xebia/java8/refactoring/step0/FunctionGeneratorTest.java
@@ -2,6 +2,8 @@ package fr.xebia.java8.refactoring.step0;
 
 import org.junit.Test;
 
+import java.util.Calendar;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -26,7 +28,49 @@ public class FunctionGeneratorTest {
     }
 
     @Test
-    public void should_return_yesterday_date() {
-        assertThat(FunctionGenerator.todaySupplier().get()).isToday();
+    public void should_increment_int_array_values() {
+        // Given
+        int[] values = {1, 2, 3, 4, 5};
+
+        // When
+        FunctionGenerator.incrementArrayValueConsumer().accept(values);
+
+        // Then
+        assertThat(values).isEqualTo(new int[]{2, 3, 4, 5, 6});
+    }
+
+    @Test
+    public void should_return_day_of_month() {
+        // When
+        int dayOfMonth = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+
+        // Then
+        assertThat(FunctionGenerator.todaySupplier().get()).isEqualTo(dayOfMonth);
+    }
+
+    @Test
+    public void should_contact_values() {
+        // Given
+        String name = "Java";
+        Integer version = 8;
+
+        // when
+        String result = FunctionGenerator.concatAsStringFunction().apply(name, version);
+
+        // Then
+        assertThat(result).isEqualTo("Java 8");
+    }
+
+    @Test
+    public void should_sum_values() {
+        // Given
+        Integer firstNumber = 3;
+        Integer secondNumber = 4;
+
+        // When
+        Integer result = FunctionGenerator.sumFunction().apply(firstNumber, secondNumber);
+
+        // Then
+        assertThat(result).isEqualTo(7);
     }
 }
