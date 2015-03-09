5550ec664a130014ad728151c3543f3208a8640d
ibeauvais <ivan.beauvais@gmail.com>
2015-03-09 10:12:32 +0100

    solutions

--- src/main/java/fr/xebia/java8/refactoring/step0/FunctionGenerator.java
+++ src/main/java/fr/xebia/java8/refactoring/step0/FunctionGenerator.java
@@ -11,100 +11,53 @@ public class FunctionGenerator {
 
     public static Function<Integer, Integer> plusOneFunction() {
         //TODO: replace with lambda syntax
-        return new Function<Integer, Integer>() {
-
-            @Override
-            public Integer apply(Integer value) {
-                return value + 1;
-            }
-        };
+        return value -> value + 1;
     }
 
     public static Predicate<Integer> evenNumberPredicate() {
         //TODO: replace with lambda syntax
-        return new Predicate<Integer>() {
-            @Override
-            public boolean test(Integer integer) {
-                return integer % 2 == 0;
-            }
-        };
+        return integer -> integer % 2 == 0;
     }
 
     public static Consumer<int[]> incrementArrayValueConsumer() {
         //TODO: replace with lambda syntax
-        return new Consumer<int[]>() {
-            @Override
-            public void accept(int[] arrayValues) {
-                for (int i = 0; i < arrayValues.length; i++) {
-                    arrayValues[i] = arrayValues[i] + 1;
+        return arrayValues -> {
+            for (int i = 0; i < arrayValues.length; i++) {
+                arrayValues[i] = arrayValues[i] + 1;
 
-                }
             }
         };
     }
 
     public static Supplier<Integer> todaySupplier() {
         //TODO: replace with lambda syntax
-        return new Supplier<Integer>() {
-            @Override
-            public Integer get() {
-                return LocalDate.now().get(ChronoField.DAY_OF_MONTH);
-
-            }
-        };
+        return () -> LocalDate.now().get(ChronoField.DAY_OF_MONTH);
     }
 
     public static BiFunction<String, Integer, String> concatAsStringFunction() {
         //TODO: replace with lambda syntax
-        return new BiFunction<String, Integer, String>() {
-
-            @Override
-            public String apply(String firstValue, Integer secondValue) {
-                return firstValue + " " + secondValue;
-            }
-        };
+        return (firstValue, secondValue) -> firstValue + " " + secondValue;
     }
 
     // BinaryOperator is BiFunction with same generic type
     public static BinaryOperator<Integer> sumFunction() {
         //TODO: replace with static method reference
-        return new BinaryOperator<Integer>() {
-
-            @Override
-            public Integer apply(Integer firstValue, Integer secondValue) {
-                return Integer.sum(firstValue, secondValue);
-            }
-        };
+        return Integer::sum;
     }
 
     public static Consumer<String> addToListConsumer(List<String> list) {
         //TODO: replace with instance method reference
-        return new Consumer<String>() {
-            @Override
-            public void accept(String value) {
-                list.add(value);
-            }
-        };
+        return list::add;
     }
 
     public static Function<String, BigDecimal> stringToBiDecimalFunction() {
         //TODO: replace with constructor reference
-        return new Function<String, BigDecimal>() {
-            @Override
-            public BigDecimal apply(String value) {
-                return new BigDecimal(value);
-            }
-        };
+        return BigDecimal::new;
     }
 
     // Comparator is also a FunctionalInterface that can be replaced by lambda syntax
     public static Comparator<String> byStringLengthComparator() {
         //TODO: replace with lambda syntax
-        return new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return Integer.compare(o1.length(), o2.length());
-            }
-        };
+        return (o1, o2) -> Integer.compare(o1.length(), o2.length());
     }
 }
