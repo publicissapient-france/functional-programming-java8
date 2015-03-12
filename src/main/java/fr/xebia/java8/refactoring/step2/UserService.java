875b19802f38a2cfdd70dd555e8176ebe64f21a7
njozwiak <njozwiak@xebia.fr>
2015-03-12 18:32:21 +0100

    Add step2 solution

--- src/main/java/fr/xebia/java8/refactoring/step2/UserService.java
+++ src/main/java/fr/xebia/java8/refactoring/step2/UserService.java
@@ -1,12 +1,18 @@
 package fr.xebia.java8.refactoring.step2;
 
 
+import java.util.Comparator;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import fr.xebia.java8.refactoring.data.Address;
 import fr.xebia.java8.refactoring.data.Role;
 import fr.xebia.java8.refactoring.data.User;
 import fr.xebia.java8.refactoring.data.UsersAgeStatistic;
 import fr.xebia.java8.refactoring.other.UserParser;
-
-import java.util.*;
 
 public class UserService {
 
@@ -19,119 +25,59 @@ public class UserService {
 
     //TODO: convert users List to stream and use filter and count
     public long countUserWithRole(Role role) {
-        long count = 0;
-        for (User user : users) {
-            if (user.getRole() == role) {
-                count++;
-            }
-        }
-
-        return count;
+        return users.stream()
+                    .filter(user -> user.getRole() == role)
+                    .count();
     }
 
     //TODO: use anyMatch
     public boolean isLoginAlreadyExist(String login) {
-        for (User user : users) {
-            if (user.getLogin().equals(login)) {
-                return true;
-            }
-        }
-
-        return false;
+        return users.stream()
+                    .anyMatch(user -> user.getLogin().equals(login));
     }
 
-    //TODO:  user OptionalAddress instead of address, use filter and findFirst.
-    //TODO: for finish this refactoring you need Optional.Map, Stream.flatMap and Optional.orElse
+    //TODO: user OptionalAddress instead of address, use filter and findFirst.
+    //TODO: to finish this refactoring you need Stream.flatMap, Optional.Map, and Optional.orElse
     public String retrieveFormatedUserAddressByLogin(String login) {
-        for (User user : users) {
-            if (user.getLogin().equals(login)) {
-                if (user.getAddress() != null) {
-                    return user.getAddress().formatForEnveloppe();
-                }
-            }
-        }
-
-        return DEFAULT_FORMATED_ADDRESS;
+        return users.stream()
+                    .filter(user -> user.getLogin().equals(login))
+                    .findFirst()
+                    .flatMap(User::getOptionalAddress)
+                    .map(Address::formatForEnveloppe)
+                    .orElse(DEFAULT_FORMATED_ADDRESS);
     }
 
     /**
      * @return first 50 users ordered by first name and last name
      */
-    //TODO: use limit, replace specific comparator with Comparator.comparing static methods and collect with Collectors for generate new List
+    //TODO: use sorted and replace specific comparator with Comparator.comparing static methods.
+    //TODO: use limit method and collect with Collectors for generate new List
     public List<User> firstFiftyUsers() {
-        List<User> usersOrdered = new ArrayList<>(users.size());
-        usersOrdered.addAll(users);
-
-        Collections.sort(usersOrdered, new UserComparator());
-
-        return usersOrdered.subList(0, 50);
+        return users.stream()
+                    .sorted(Comparator.comparing(User::getLastname).thenComparing(User::getFirstname))
+                    .limit(50)
+                    .collect(Collectors.toList());
     }
 
-    private static class UserComparator implements Comparator<User> {
-
-        public int compare(User userLeft, User userRight) {
-            int lastNameComparaison = userLeft.getLastname().compareTo(userRight.getLastname());
-            if (lastNameComparaison == 0) {
-                return userLeft.getFirstname().compareTo(userRight.getFirstname());
-            } else {
-
-                return lastNameComparaison;
-            }
-        }
-    }
-
-    //TODO: Use collect with Collectors.groupingBy
+    //TODO: Use filter on expired method and collect with Collectors.groupingBy
     public Map<Role, List<User>> retrieveActiveUserByRole() {
-        Map<Role, List<User>> result = new HashMap<>();
-
-        for (User user : users) {
-            if (!user.isExpired()) {
-
-                List<User> currentRoleUsers = result.get(user.getRole());
-                if (currentRoleUsers == null) {
-                    currentRoleUsers = new ArrayList<>();
-                    result.put(user.getRole(), currentRoleUsers);
-                }
-                currentRoleUsers.add(user);
-            }
-        }
-
-        return result;
+        return users.stream()
+                    .filter(user -> !user.isExpired())
+                    .collect(Collectors.groupingBy(User::getRole));
     }
 
-    //TODO: Use collect with Collectors.toMap and Function.identity() as value mapper
+    //TODO: Use filter on role and collect with Collectors.toMap and Function.identity() as value mapper
     public Map<String, User> retrieveUserWithRoleByLogin(Role role) {
-        Map<String, User> result = new HashMap<>();
-
-        for (User user : users) {
-            if (user.getRole() == role) {
-                result.put(user.getLogin(), user);
-            }
-        }
-
-        return result;
+        return users.stream()
+                    .filter(user -> user.getRole() == role)
+                    .collect(Collectors.toMap(User::getLogin, Function.identity()));
     }
 
     //TODO: use collect with Collectors.summarizingInt
+    //TODO: use returned IntSummaryStatistics object to create UsersAgeStatistic object
     public UsersAgeStatistic generateAgeStatistic() {
-        long count = 0;
-        int min = Integer.MAX_VALUE;
-        int max = 0;
-        int sum = 0;
+        IntSummaryStatistics summaryStatistics = users.stream().collect(Collectors.summarizingInt(User::age));
 
-        for (User user : users) {
-            int age = user.age();
-            if (age > max) {
-                max = age;
-            }
-            if (age < min) {
-                min = age;
-            }
-            count++;
-            sum += age;
-        }
-        double average = (double) sum / count;
-
-        return new UsersAgeStatistic(count, min, max, average);
+        return new UsersAgeStatistic(summaryStatistics.getCount(), summaryStatistics.getMin(), summaryStatistics.getMax(), summaryStatistics.getAverage());
     }
 }
