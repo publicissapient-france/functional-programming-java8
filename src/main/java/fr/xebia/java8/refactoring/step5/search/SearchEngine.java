1c74e7a0934a45acabddb0992a1bf36a5937682f
ibeauvais <ivan.beauvais@gmail.com>
2015-03-13 23:22:05 +0100

    add searchEngine

---
+++ src/main/java/fr/xebia/java8/refactoring/step5/search/SearchEngine.java
@@ -0,0 +1,33 @@
+package fr.xebia.java8.refactoring.step5.search;
+
+import fr.xebia.java8.refactoring.step5.Product;
+import fr.xebia.java8.refactoring.step5.repository.FakeRepositoryTools;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class SearchEngine {
+
+    private final int nodeId;
+
+    public SearchEngine(int nodeId) {
+
+        this.nodeId = nodeId;
+    }
+
+    public static SearchEngine getNode(int nodeId) {
+        return new SearchEngine(nodeId);
+    }
+
+    public Product searchByName(String name) {
+        FakeRepositoryTools.waitInSecond(1.5);
+        return new Product(1, name, name + " nodeId:"+nodeId, Product.Category.BOOKS);
+    }
+
+    public static List<SearchEngine> getNodes() {
+        return IntStream.range(0, 10)
+                .mapToObj(i -> new SearchEngine(i))
+                .collect(Collectors.toList());
+    }
+}
