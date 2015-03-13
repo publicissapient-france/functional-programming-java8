d00a4c1ec08b24850eb3312fb795baa5d5cf04d9
ibeauvais <ivan.beauvais@gmail.com>
2015-03-13 23:21:43 +0100

    toString on product

--- src/main/java/fr/xebia/java8/refactoring/step5/Product.java
+++ src/main/java/fr/xebia/java8/refactoring/step5/Product.java
@@ -53,4 +53,14 @@ public class Product {
     public void setCategory(Category category) {
         this.category = category;
     }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", category=" + category +
+                '}';
+    }
 }
