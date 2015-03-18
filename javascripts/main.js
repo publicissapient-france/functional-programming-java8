91b1447b56b1da711025b96d70eed78a16b97a3d
ibeauvais <ivan.beauvais@gmail.com>
2015-03-18 12:38:46 +0100

    reference to solution branche

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function() {
   jQuery(".help").wrap('<div></div>')
-  jQuery(".help").before('<button  type="button" class = "teaser">Aide supplémentaire</button>')
+  jQuery(".help").append('<Strong>Si vous êtes toujours bloqué vous pouvez allez regarder sur la branche solution:</strong> <br/> <code>git checkout step*-solution</code>')
+  jQuery(".help").before('<button  type="button" class = "teaser">Solutions</button>')
   jQuery(".help").hide();
   jQuery(document).on('click', '.teaser', function() {
     jQuery(this).next(".help").slideToggle(500);
